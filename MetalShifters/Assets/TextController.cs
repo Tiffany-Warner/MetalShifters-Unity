ae7056d9821d30baa5813c2c14d06112deaf7184
Tiffany Warner <tiffy.warner@gmail.com>
2018-03-02 21:27:59 -0700

    Initial Commit - Setup Files for Unity

---
+++ MetalShifters/Assets/TextController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextController : MonoBehaviour {
+
+	//Access modifier so we can change text value in Unity
+	public Text text; 
+
+	// Use this for initialization
+	void Start () {
+		text.text = "Hello World.";
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			text.text = "Space key pressed.";
+		}
+	}
+}
